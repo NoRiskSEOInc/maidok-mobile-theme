792eca8e89d0620110cb1c30aa745c6607360aa9
Steven C. Spencer <steven@thewebsitesupply.com>
2014-10-24 17:40:05 -0400

    Maid OK - Mobile Theme Dev - File Systems Updates

---
+++ wp-content/themes/twentyfourteen/footer.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+			<?php get_sidebar( 'footer' ); ?>
+
+			<div class="site-info">
+				<?php do_action( 'twentyfourteen_credits' ); ?>
+				&copy; 2014 Maid OK - All Rights Reserved
+
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+
+
+
+
+<script type=”text/javascript”>
+var __lc = {};
+__lc.license = 5150451;
+(function() {
+var lc = document.createElement(‘script’); lc.type = ‘text/javascript’;
+lc.async = true;
+lc.src = (‘https:’ == document.location.protocol ? ‘https://’; : ‘http://’;)
++ ‘cdn.livechatinc.com/tracking.js’;
+var s = document.getElementsByTagName(‘script’)[0];
+s.parentNode.insertBefore(lc, s);
+})();
+</script>
+
+
+
+
+
+
+<!-- Google Code for Remarketing Tag -->
+<!--------------------------------------------------
+Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
+--------------------------------------------------->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 968102486;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/968102486/?value=0&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+</body>
+</html>
---
+++ wp-content/themes/twentyfourteen/header.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * The Header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<meta name="google-site-verification" content="8tqsxQVpLDUPPE8QoKDB3fZ6RqtRzjSIgKcUsB89Ep8" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+<script async src="//18110.tctm.co/t.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50751922-1', 'maidok.com');
+  ga('send', 'pageview');
+
+</script>
+
+
+<meta name="msvalidate.01" content="3ACE1910A63C8B23A6FE2F7F1A7ABF3D" />
+
+
+
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+
+
+   				<div id="MarqueeWrapper" >
+					<marquee scrollamount="2" loop="infinite" scrolldelay="2">IF YOU HAVE NEVER USED OUR SERVICES OR HAVE NOT USED OUR SERVICES FOR THE PAST 3 MONTHS, BOOK ANY SERVICE, SAY YOU SAW THE MARQUEE ON THE WEBSITE AND RECEIVE 10% OFF!!</span><div style="clear: both;">
+</marquee>
+				</div><!-- // MarqueeWrapper -->
+
+		<img class="slideshow_overlay" src="/wp-content/themes/twentyfourteen/images/slideshow_overlay.png">
+
+	<div id="slideshow">
+
+			<?php do_action('slideshow_deploy', '5'); ?>
+	
+	</div>
+
+	<?php if ( get_header_image() ) : ?>
+	<div id="site-header">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+		</a>
+	</div>
+	<?php endif; ?>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="header-main">
+			
+		
+
+			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+				<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
+				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+			</nav>
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="main" class="site-main">
